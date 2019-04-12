01327269d1c2afa597abbab5471360a52eaedabe
Diala Rbeiz <diala21@hotmail.com>
2019-04-12 11:25:50 -0700

    Adjust spacing

--- bikeshare.py
+++ bikeshare.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('Which city (chicago, new york city, washington) would you like to analyze? ').lower()
@@ -38,6 +39,7 @@ def get_filters():
             break;
 
     print('-'*40)
+    
     return city, month, day
 
 
@@ -98,6 +100,7 @@ def time_stats(df):
     print('The most common start hour is: {}'.format(common_start_hour))
     
     print("\nThis took %s seconds." % (time.time() - start_time))
+    
     print('-'*40)
 
 
